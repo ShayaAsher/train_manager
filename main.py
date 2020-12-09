7a56623fb627720ab674a059a1c702feed7e0f93
ShayaAsher <teem1israel@gmail.com>
2020-12-09 20:33:19 +0200

    add_save_data

--- main.py
+++ main.py
@@ -4,3 +4,13 @@ def load_data():
     with open("data.json", "r") as read_file:
         data = json.load(read_file)
     return data
+
+def save_data(data):
+    with open("data.json", "w") as write_file:
+        json.dump(data, write_file)
+
+# data = load_data()
+
+# data["name"] = "Bob"
+
+# save_data(data)
